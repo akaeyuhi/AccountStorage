a6242868c6e6941bed7a8da34ff325656671404b
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-21 21:47:04 +0200

    Implemented AuthStrategy for project

---
+++ src/server/src/auth/auth.strategy.ts
@@ -0,0 +1,23 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload) {
+    return {
+      id: payload.id,
+      email: payload.email,
+      role: payload.role,
+    };
+  }
+}
