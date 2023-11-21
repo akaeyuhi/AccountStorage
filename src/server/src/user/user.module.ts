4e20514497dc4d86c696a72cd2827ef1d0ca8683
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-21 21:44:29 +0200

    Added Typeorm and Multer to imports

--- src/server/src/user/user.module.ts
+++ src/server/src/user/user.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    MulterModule.register({
+      dest: '/static',
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
