02f751f16e3986ed99728a27a9bb343a9b0c572e
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-21 21:48:18 +0200

    Fixed imports

--- src/server/src/auth/auth.module.ts
+++ src/server/src/auth/auth.module.ts
@@ -3,17 +3,26 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import { AuthStrategy } from './auth.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    UserService,
+    UserModule,
+    PassportModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       global: true,
-      secret: process.env.JWT_SECRER,
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '24h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserService, AuthStrategy],
 })
 export class AuthModule {}
