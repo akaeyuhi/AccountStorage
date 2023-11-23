7713840939ea8631551a20d45edac4c992d0352f
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-23 04:59:35 +0200

    Added page routing

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router';
+import LoginPage from 'pages/Login';
+import ErrorMessage from 'utils/components/ErrorMessage';
+import { useAppSelector } from 'app/hooks';
+import HomePage from 'pages/Home';
 
 function App() {
-  return <div className="App"></div>;
+  const navigate = useNavigate();
+  const { token } = useAppSelector(state => state.account);
+
+  useEffect(() => {
+    if (token) {
+      navigate('/');
+    } else {
+      navigate('/login');
+    }
+  }, [token]);
+
+  return <div className="container">
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/' element={<HomePage />} />
+      <Route path='*' element={
+        <main className="main main__welcome">
+          <ErrorMessage error={new Error('404 not found')}/>
+        </main>} />
+    </Routes>
+  </div>;
 }
 
 export default App;
