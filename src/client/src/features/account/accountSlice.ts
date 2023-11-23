e4ae1e8f3b5fde76c3fe9fe256770d92f5b71094
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-23 03:24:46 +0200

    added register case

--- src/client/src/features/account/accountSlice.ts
+++ src/client/src/features/account/accountSlice.ts
@@ -2,6 +2,7 @@ import { Action, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { loginUser } from './actions/loginUser';
 import { AuthState } from './types/AuthState';
 import { AuthResponse } from './types/AuthResponse';
+import { registerUser } from 'features/account/actions/registerUser';
 
 const storageNames = {
   user: 'loginUser',
@@ -21,6 +22,7 @@ const isPendingAction = (action: Action) => action.type.endsWith('pending');
 const saveUser = (state: AuthState, action: PayloadAction<AuthResponse>) => {
   state.loading = false;
   state.token = action.payload.accessToken;
+  state.user = action.payload.user;
   localStorage.setItem(storageNames.token, state.token as string);
 };
 
@@ -36,6 +38,9 @@ const accountSlice = createSlice({
   },
   extraReducers: builder => {
     builder.addCase(loginUser.fulfilled, (state, action) => {
+      saveUser(state, action);
+    });
+    builder.addCase(registerUser.fulfilled, (state, action) => {
       saveUser(state, action);
     });
     builder.addMatcher(isRejectedAction, (state, action) => {
