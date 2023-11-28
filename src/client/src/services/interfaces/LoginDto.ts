35aa9f3d21c2afa8c96572db1ebdac7469c6366a
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-28 04:51:41 +0200

    Minor update for LoginDto.ts

--- src/client/src/services/interfaces/LoginDto.ts
+++ src/client/src/services/interfaces/LoginDto.ts
@@ -1,4 +1,6 @@
-export interface LoginDto {
+import { RegisterDto } from 'services/interfaces/RegisterDto';
+
+export interface LoginDto extends Partial<RegisterDto>{
   email: string;
   password: string;
 }
