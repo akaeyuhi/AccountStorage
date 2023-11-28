5587e23672673fddc50ff7dc266dd0c55051479d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-28 04:53:47 +0200

    Added userService to createAppAsyncThunk

--- src/client/src/app/createAsyncThunk.ts
+++ src/client/src/app/createAsyncThunk.ts
@@ -1,10 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import AuthService from '../services/AuthService';
 import { AppDispatch, RootState } from './store';
+import UserService from 'services/UserService';
 
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
   state: RootState;
   dispatch: AppDispatch;
   rejectValue: string;
-  extra: { authService: AuthService };
+  extra: { authService: AuthService, userService: UserService };
 }>();
