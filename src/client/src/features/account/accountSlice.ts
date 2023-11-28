25bd673de9462b5a962f84dd402dde8bb4f8c121
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-11-28 04:55:57 +0200

    Fixed matchers in accountSlice.ts

--- src/client/src/features/account/accountSlice.ts
+++ src/client/src/features/account/accountSlice.ts
@@ -1,4 +1,4 @@
-import { Action, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, isPending, isRejected, PayloadAction } from '@reduxjs/toolkit';
 import { loginUser } from './actions/loginUser';
 import { AuthState } from './types/AuthState';
 import { AuthResponse } from './types/AuthResponse';
@@ -16,14 +16,15 @@ const initialState: AuthState = {
   error: null,
 };
 
-const isRejectedAction = (action: Action) => action.type.endsWith('rejected');
-const isPendingAction = (action: Action) => action.type.endsWith('pending');
+const isRejectedAction = () => isRejected(loginUser, registerUser);
+const isPendingAction = () => isPending(loginUser, registerUser);
 
 const saveUser = (state: AuthState, action: PayloadAction<AuthResponse>) => {
   state.loading = false;
   state.token = action.payload.accessToken;
   state.user = action.payload.user;
   localStorage.setItem(storageNames.token, state.token as string);
+  localStorage.setItem(storageNames.user, JSON.stringify(state.user));
 };
 
 const accountSlice = createSlice({
@@ -32,6 +33,7 @@ const accountSlice = createSlice({
   reducers: {
     logout(state: AuthState) {
       state.token = null;
+      state.user = null;
       localStorage.removeItem(storageNames.user);
       localStorage.removeItem(storageNames.token);
     },
@@ -43,11 +45,11 @@ const accountSlice = createSlice({
     builder.addCase(registerUser.fulfilled, (state, action) => {
       saveUser(state, action);
     });
-    builder.addMatcher(isRejectedAction, (state, action) => {
+    builder.addMatcher(isRejectedAction(), (state, action) => {
       state.loading = false;
-      state.error = action.payload;
+      state.error = action.payload ?? new Error('Internal server error. Try again later.');
     });
-    builder.addMatcher(isPendingAction, state => {
+    builder.addMatcher(isPendingAction(), state => {
       state.loading = true;
       state.error = null;
     });
